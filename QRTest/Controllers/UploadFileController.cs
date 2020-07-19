8c5c3117f85018c0ab44f0b8b3e6d236537ee7c8
Iulian <iulian.astalus@gmail.com>
2020-07-19 20:55:08 +0300

    Add project files.

---
+++ QRTest/Controllers/UploadFileController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using QRTest.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace QRTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadFileController : ControllerBase
+    {
+        public UploadFileController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private readonly IConfiguration Configuration;
+        [RequestSizeLimit(1048576)]
+        public async Task<List<UploadFileResponse>> Upload([FromForm]IFormFile file)
+        {
+            string url = Configuration["RemoteServiceURL"];
+            byte[] fileBytes;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        file.CopyTo(ms);
+                        fileBytes = ms.ToArray();
+                    }
+                    client.BaseAddress = new Uri(url);
+                    using (var content = new MultipartFormDataContent())
+                    {
+                        var imageContent = new ByteArrayContent(fileBytes);
+                        content.Add(imageContent, file.Name, file.FileName);
+                        var responseMessage = await client.PostAsync(url, content);
+                        var response = await responseMessage.Content.ReadAsStringAsync();
+                        return JsonConvert.DeserializeObject<List<UploadFileResponse>>(response);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return new List<UploadFileResponse>
+                { 
+                    new UploadFileResponse
+                    {
+                        Symbol = new List<UploadFileResponse.ResponseSymbol>
+                        {
+                            new UploadFileResponse.ResponseSymbol
+                            {
+                                Error ="Server Error"
+                            }
+                        }
+                    }                    
+                };
+            }
+        }        
+    }
+}
---
+++ QRTest/Models/UploadFileResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRTest.Models
+{
+    public class UploadFileResponse
+    {
+        public string Type { get; set; }
+        public List<ResponseSymbol> Symbol { get; set; }
+
+        public class ResponseSymbol
+        {
+            public int Seq { get; set; }
+            public string Data { get; set; }
+            public string Error { get; set; }
+        }
+
+    }
+}
